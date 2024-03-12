b162afa03909a4ba7f582bc33ab1a27132613180
Kuba663 <kuba307@poczta.onet.pl>
2024-03-12 19:04:19 +0100

    Integracja z EnderIO

--- src/main/java/pl/net/eerie/tictweaks/integration/IntegrationEnderIO.java
+++ src/main/java/pl/net/eerie/tictweaks/integration/IntegrationEnderIO.java
@@ -1,22 +1,46 @@
 package pl.net.eerie.tictweaks.integration;
 
-//import net.minecraft.entity.EntityCreature;
-//import net.minecraft.entity.IEntityOwnable;
 
-//import java.util.function.Supplier;
+import crazypants.enderio.base.item.soulvial.ItemSoulVial;
+import crazypants.enderio.util.CapturedMob;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.jetbrains.annotations.NotNull;
+import slimeknights.mantle.util.RecipeMatch;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class IntegrationEnderIO {
-    /*public static class EntityOwnableSupplier implements Supplier<IEntityOwnable> {
+    public static class CapturedMobRecipeMatch extends RecipeMatch {
 
-        private final EntityCreature entity;
+        @GameRegistry.ObjectHolder("enderio:item_soul_vial")
+        public static final ItemSoulVial SOUL_VIAL = null;
 
-        public EntityOwnableSupplier(EntityCreature entity) {
+        private final ResourceLocation entity;
+
+        public CapturedMobRecipeMatch(ResourceLocation entity) {
+            super(1, 1);
             this.entity = entity;
         }
 
         @Override
-        public IEntityOwnable get() {
-            return new EntityWrapper(entity);
+        public List<ItemStack> getInputs() {
+            return Collections.singletonList(SOUL_VIAL.createVesselWithEntityStub(this.entity));
         }
-    }*/
+
+        @Override
+        public Optional<Match> matches(@NotNull NonNullList<ItemStack> nonNullList) {
+            for (ItemStack stack : nonNullList) {
+                if (stack.getItem() instanceof ItemSoulVial
+                        && CapturedMob.containsSoul(stack)
+                        && CapturedMob.create(this.entity).isSameType(CapturedMob.create(stack)))
+                    return Optional.of(new Match(Collections.singletonList(stack), 1));
+            }
+            return Optional.empty();
+        }
+    }
 }
--- src/main/java/pl/net/eerie/tictweaks/traits/enderio/SoulfulTrait.java
+++ src/main/java/pl/net/eerie/tictweaks/traits/enderio/SoulfulTrait.java
@@ -1,26 +1,31 @@
 package pl.net.eerie.tictweaks.traits.enderio;
 
-import crazypants.enderio.util.CapturedMob;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.NotNull;
 import pl.net.eerie.tictweaks.TiCTweaks;
 import pl.net.eerie.tictweaks.config.SoulTraitsConfig;
+import pl.net.eerie.tictweaks.integration.IntegrationEnderIO;
+import slimeknights.mantle.util.RecipeMatch;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.modifiers.ModifierAspect;
-import slimeknights.tconstruct.library.modifiers.ModifierNBT;
 import slimeknights.tconstruct.library.modifiers.ModifierTrait;
 import slimeknights.tconstruct.library.modifiers.TinkerGuiException;
 import slimeknights.tconstruct.library.utils.TagUtil;
 
+import java.util.Optional;
+
 public class SoulfulTrait extends ModifierTrait {
 
     private final EntityEntry entity;
+
+    private final IntegrationEnderIO.CapturedMobRecipeMatch recipeMatch;
 
     public SoulfulTrait(@NotNull EntityEntry entity) {
         super(String.format("soul_%s", entity.getRegistryName().getPath()), entity.getEgg().primaryColor);
@@ -37,9 +42,13 @@ public class SoulfulTrait extends ModifierTrait {
                nbtTagCompound1.setString("soul", entity.getRegistryName().toString());
             }
         });
+        this.recipeMatch = new IntegrationEnderIO.CapturedMobRecipeMatch(entity.getRegistryName());
     }
 
-
+    @Override
+    public Optional<RecipeMatch.Match> matches(NonNullList<ItemStack> stacks) {
+        return this.recipeMatch.matches(stacks);
+    }
 
     @Override
     public String getLocalizedName() {
